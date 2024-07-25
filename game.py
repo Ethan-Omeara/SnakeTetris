1e794ff832d64f3f0269c47489ac4ba81415aea9
EO4537 <ethan.omeara@hotmail.com>
2024-07-26 11:52:55 +1200

    Modular GUI

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-
-
 class GameController:
     def __init__(self, width=3, height=2) -> None:
         # Note to self: board 2d list is formatted so you can do board[x][y]
@@ -7,6 +5,7 @@ class GameController:
         self.snake = [[0, 1]]
         self.board[0][1] = 1
         self.dir = [1, 0] # Steps for the snake, x, y
+        self.events = []
     
     def __str__(self) -> str:
         board_str=''
@@ -17,34 +16,31 @@ class GameController:
             board_str = board_str+'\n'
         return board_str
     
-    def step(self) -> int:
-        """Run through one loop of the game, return status
-        0 - OK
-        1 - Game End"""
+    def step(self) -> None:
+        """Run through one loop of the game"""
         # Get head position
         head = self.snake[0]
         # Get new snake head position
         new_head = [head[0] + self.dir[0], head[1] + self.dir[1]]
-        print(not new_head[0] in range(len(self.board)))
         if not (new_head[0] in range(len(self.board)) or not new_head[1] in range(len(self.board[0]))):
-            return 1
+            self.call_event("end_snake")
         self.snake.insert(0, new_head)
         # Update board
         self.board[new_head[0]][new_head[1]] = 1
         # Remove the end of the snake
         self.board[self.snake[-1][0]][self.snake[-1][1]] = 0
         self.snake.pop()
-        return 0
+        
+        self.call_event("draw_board")
     
-    def end_game(self) -> list:
-        return self.board
+    def create_event(self, event: str, func: callable) -> None:
+        """Create a game event to be called on certain conditions.
+        Current events:
+            draw_board - Sends a copy of the snake board to draw
+            end_snake - Ends the snake portion of the game"""
+        self.events.append([event, func])
     
-
-
-# test = GameController()
-
-# while True:
-#     print(test.snake)
-#     print(test)
-#     input()
-#     print(test.step())
+    def call_event(self, event_name: str, arg=None) -> None:
+        for event in self.events:
+            if event[0] == event_name:
+                event[1](arg)
--- main.py
+++ main.py
@@ -3,39 +3,15 @@ Author: Ethan O'Meara
 Date: 25/06/2024
 Description: A python program using Tkinter to run 
              a game inspired by Snake and Tetris
-
-Changes:
-Version 1 - Basic Menu Design
 """
-
+# Get relevant dictionaries
 from tkinter import *
 from tkinter import ttk
 
-import game
+from game import GameController
+import gui
 
-CELL_SIZE = 50
+def play():
+    pass
 
-root = Tk()
-root.title("Snake-Tetris")
-
-root.columnconfigure(list(range(0, 8)), minsize=CELL_SIZE)
-root.rowconfigure(list(range(0, 11)), minsize=CELL_SIZE)
-
-# Draw Tetris
-Frame(root, bg="#1515B9").grid(row=3, column=3, rowspan=2, columnspan=2, sticky="NSEW")
-Label(root, bg="#1515B9", fg="#FFFFFF", text="T E T R I S", font=("System", 40, "bold")).grid(row=1, column=1, rowspan=2, columnspan=6, sticky="NSEW")
-
-# Draw Snake
-Label(root, bg="#34DE34", text=".        .", font=("System", 15, "bold")).grid(row=7, column=6, rowspan=1, columnspan=1, sticky="NSEW")
-Button(root, bg="#FF0000", fg="#FFFFFF", text="X", font=("System", 20, "bold"), command=exit).grid(row=9, column=6, rowspan=1, columnspan=1, sticky="NSEW")
-Label(root, bg="#34DE34", fg="#000000", text="S N A K E", font=("System", 30, "bold")).grid(row=6, column=1, rowspan=1, columnspan=6, sticky="NSEW")
-
-# Buttons
-Button(root, bg="#7F7F7F", fg="#FFFFFF", text="PLAY", font=("System", 20, "bold")).grid(row=8, column=1, columnspan=4, sticky="NSEW")
-Button(root, bg="#7F7F7F", fg="#FFFFFF", text="SCORES", font=("System", 15, "bold")).grid(row=9, column=1, columnspan=2, sticky="NSEW")
-Button(root, bg="#7F7F7F", fg="#FFFFFF", text="CREDITS", font=("System", 15, "bold")).grid(row=9, column=3, columnspan=2, sticky="NSEW")
-
-root.minsize(50*8, 50*11)
-root.maxsize(50*8, 50*11)
-
-root.mainloop()
+gui.Menu(exit, play)
